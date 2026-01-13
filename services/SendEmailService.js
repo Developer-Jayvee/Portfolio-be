c6a51eb0a27080f5e4f1f51eae309ab4403a8f55
Developer-Jayvee <jayvee.github0703@gmail.com>
2026-01-13 15:10:15 +0800

    Added resend

--- services/SendEmailService.js
+++ services/SendEmailService.js
@@ -1,33 +1,44 @@
 
 const axios = require('axios')
 const nodemailer = require("nodemailer");
+const { Resend } = require('resend');
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: 587,
-  secure: false, 
-  auth: {
-    user: process.env.SMTP_USERNAME,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY)
+// const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: 2587,
+//   secure: false, 
+//   auth: {
+//     user: process.env.SMTP_USERNAME,
+//     pass: process.env.SMTP_PASSWORD,
+//   },
+//   connectionTimeout: 10000,
+// });
 async function sendEmail(email_data){
     let response = { message: 'Failed to send email' , status : 0 }
     const request = JSON.parse(email_data);
-    
-    const info = await transporter.sendMail({
-        from: process.env.RECEIPIENT_EMAIL,
-        to: process.env.RECEIPIENT_EMAIL,
-        subject: "Portfolio Email Send",
-        text: '',
-        html:`<h1>From : ${request.email} </h1> <br> ${request.message}` 
+
+    const { data, error } = await resend.emails.send({
+        from: 'Jayvee Portfolio <onboarding@resend.dev>',
+        to:'jayvee.github0703@gmail.com',
+        subject: "hello world",
+        html: "<strong>it works!</strong>",
     });
-  if(info){
-    response.message = 'Success';
-    response.status = 1
-    return response
-  }
-  return response
+
+    return data;
+  //   const info = await transporter.sendMail({
+  //       from: process.env.RECEIPIENT_EMAIL,
+  //       to: process.env.RECEIPIENT_EMAIL,
+  //       subject: "Portfolio Email Send",
+  //       text: '',
+  //       html:`<h1>From : ${request.email} </h1> <br> ${request.message}` 
+  //   });
+  // if(info){
+  //   response.message = 'Success';
+  //   response.status = 1
+  //   return response
+  // }
+  // return response
 }
 module.exports = { sendEmail };
 
