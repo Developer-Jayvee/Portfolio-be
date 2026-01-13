2fe455d952b184161c17d5bef7ffc5bc60ccabc7
Developer-Jayvee <jayvee.github0703@gmail.com>
2026-01-13 14:32:37 +0800

    Change nodemailer port

--- services/SendEmailService.js
+++ services/SendEmailService.js
@@ -4,6 +4,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
+  port: 465,
+  secure: true, 
   auth: {
     user: process.env.SMTP_USERNAME,
     pass: process.env.SMTP_PASSWORD,
